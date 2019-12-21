92a84b0bdcad132f9718eed1cf96dbd275ff1d7f
Ashura <the.lucas2016@gmail.com>
2019-12-21 17:08:31 -0200

    Criar controller, model, factory, migration & seeder das notas

---
+++ api/database/factories/NoteFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Note;
+use Faker\Generator as Faker;
+
+$factory->define(Note::class, function (Faker $faker) {
+    return [
+        'title' => $faker->text(32),
+        'body' => $faker->text(200)
+    ];
+});
